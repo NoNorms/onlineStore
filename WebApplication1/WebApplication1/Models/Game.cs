bd18b8a2c601b7c5fb106a79fc274623766ee5d6
NoNorms <nonorms@yandex.ru>
2018-12-05 12:49:02 +0300

    q

---
+++ WebApplication1/WebApplication1/Models/Game.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class Game
+    {
+        public int id { get; set; }
+        public string title { get; set; }
+        public string genre { get; set; }
+        public decimal price { get; set; }
+    }
+}
